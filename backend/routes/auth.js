76187ca8a025b6db248e308650c0bc3ddb99ade0
Uzair-ahmed2920 <uzairshaikh2920@gmail.com>
2022-06-21 15:31:46 +0500

    validation done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,12 +2,26 @@
 const express = require('express');
 const User = require('../models/User');
 const router = express.Router();
-router.get('/',(req, res)=>
+const { body, validationResult } = require('express-validator');
+
+router.post('/',[
+   
+   body('name').isLength({ min: 5 }),
+   body('email').isEmail(),
+   body('password').isLength({ min: 5 }),
+
+],(req, res)=>
 {
-   console.log(req.body);
-   const user = User(req.body);
-   user.save();
-   res.send(req.body);
+   const errors = validationResult(req);
+   if (!errors.isEmpty()) {
+     return res.status(400).json({ errors: errors.array() });
+   }
+
+   User.create({
+     name: req.body.name,
+     email: req.body.email,
+     password: req.body.password,
+   }).then(user => res.json(user));
 })
 
 module.exports = router
