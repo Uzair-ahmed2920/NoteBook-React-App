b4326c3d816511dcafeec01f9dee2e8a2443feb8
Uzair-ahmed2920 <uzairshaikh2920@gmail.com>
2022-06-23 16:40:54 +0500

    error handling

--- backend/models/User.js
+++ backend/models/User.js
@@ -18,5 +18,4 @@ const UserSchema = new Schema({
    
   });
   const User= mongoose.model('user', UserSchema);
-  User.createIndexes();
   module.exports = User
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,27 +1,46 @@
-
-const express = require('express');
-const User = require('../models/User');
+const express = require("express");
+const User = require("../models/User");
 const router = express.Router();
-const { body, validationResult } = require('express-validator');
+const { body, validationResult } = require("express-validator");
 
-router.post('/',[
-   
-   body('name').isLength({ min: 5 }),
-   body('email').isEmail(),
-   body('password').isLength({ min: 5 }),
+// creating new user using post : "/api/auth/createuser"
+router.post(
+  "/createuser",
+  [
+    body("name").isLength({ min: 5 }),
+    body("email").isEmail(),
+    body("password").isLength({ min: 5 }),
+  ],
+  async (req, res) => {
+    //eroor handling for bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    // handling erroe for user with same email id
+    try{
+    let user = await User.findOne({ email: req.body.email });
+    if (user) {
+      return res
+        .status(400)
+        .json({ error: "sorry a user with same email already exixst" });
+    }
+    //creating new user
+    user = await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    res.json(user);
+  }
+  catch (error)
+  {
+    console.error(error.message);
+    res.status(500).send("some error occoured");
+  }
 
-],(req, res)=>
-{
-   const errors = validationResult(req);
-   if (!errors.isEmpty()) {
-     return res.status(400).json({ errors: errors.array() });
-   }
+    //.then(user => res.json(user));
+  }
+);
 
-   User.create({
-     name: req.body.name,
-     email: req.body.email,
-     password: req.body.password,
-   }).then(user => res.json(user));
-})
-
-module.exports = router
+module.exports = router;
