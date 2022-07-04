79fa75bfe753a9ab2b79518afeb45ab7ce201ffb
Uzair-ahmed2920 <uzairshaikh2920@gmail.com>
2022-07-04 16:47:25 +0500

    Authenticating user login done

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -47,7 +47,7 @@ router.post(
       };
       // generating authtoken
       const authtoken = jwt.sign(data, JWT_SECRET);
-      res.json({authtoken});
+      res.json({ authtoken });
     } catch (error) {
       console.error(error.message);
       res.status(500).send("some error occoured");
@@ -56,4 +56,45 @@ router.post(
   }
 );
 
+// Authenticating user using post : "/api/auth/login"
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "password can not be blanck").exists(),
+  ],
+  async (req, res) => {
+    //eroor handling for bad request
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({email});
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials" });
+      }
+      let compPassword = await bcrypt.compare(password, user.password);
+      if (!compPassword) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials" });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      // generating authtoken
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("some error occoured");
+    }
+  }
+);
 module.exports = router;
